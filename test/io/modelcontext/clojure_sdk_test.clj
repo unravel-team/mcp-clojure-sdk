535f00914f0a2d5500632245e85cd81e910d5f64
Vedang Manerikar <ved.manerikar@gmail.com>
2025-02-06 14:12:33 +0530

    Initial Commit: In the beginning, there was darkness

---
+++ test/io/modelcontext/clojure_sdk_test.clj
@@ -0,0 +1,7 @@
+(ns io.modelcontext.clojure-sdk-test
+  (:require [clojure.test :refer :all]
+            [io.modelcontext.clojure-sdk :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
