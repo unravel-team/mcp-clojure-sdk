4146daf865a48b3af4fdd23a3e6b18f33ab9002c
Vedang Manerikar <ved.manerikar@gmail.com>
2025-03-11 08:07:16 +0530

    examples: Update build

--- examples/build.clj
+++ examples/build.clj
@@ -2,36 +2,41 @@
   (:refer-clojure :exclude [test])
   (:require [clojure.tools.build.api :as b]))
 
-(def lib 'net.clojars.examples/examples)
-(def version "0.1.0-SNAPSHOT")
-(def main 'examples.examples)
+(def lib 'io.modelcontextprotocol.clojure-sdk/examples)
+(def version (format "1.0.%s" (b/git-count-revs nil)))
+(def main 'calculator-server)
 (def class-dir "target/classes")
 
-(defn test "Run all the tests." [opts]
-  (let [basis    (b/create-basis {:aliases [:test]})
-        cmds     (b/java-command
-                  {:basis     basis
-                   :main      'clojure.main
-                   :main-args ["-m" "cognitect.test-runner"]})
+(defn test
+  "Run all the tests."
+  [opts]
+  (let [basis (b/create-basis {:aliases [:test]})
+        cmds (b/java-command {:basis basis,
+                              :main 'clojure.main,
+                              :main-args ["-m" "cognitect.test-runner"]})
         {:keys [exit]} (b/process cmds)]
     (when-not (zero? exit) (throw (ex-info "Tests failed" {}))))
   opts)
 
-(defn- uber-opts [opts]
+(defn- uber-opts
+  [opts]
   (assoc opts
-         :lib lib :main main
-         :uber-file (format "target/%s-%s.jar" lib version)
-         :basis (b/create-basis {})
-         :class-dir class-dir
-         :src-dirs ["src"]
-         :ns-compile [main]))
+    :lib lib
+    :main main
+    :uber-file (format "target/%s-%s.jar" lib version)
+    :basis (b/create-basis {})
+    :class-dir class-dir
+    :src-dirs ["src"]
+    :ns-compile [main]))
 
-(defn ci "Run the CI pipeline of tests (and build the uberjar)." [opts]
+(defn ci
+  "Run the CI pipeline of tests (and build the uberjar)."
+  [opts]
   (test opts)
   (b/delete {:path "target"})
   (let [opts (uber-opts opts)]
     (println "\nCopying source...")
-    (b/copy-dir {:src-dirs ["resources" "src"] :target-dir class-dir})
+    (b/copy-dir {:src-dirs ["resources" "src"], :target-dir class-dir})
     (println (str "\nCompiling " main "..."))
     (b/compile-clj opts)
     (println "\nBuilding JAR...")
