f367fe76d573a04f2a9b8fe41b04b2b0b3eebf74
Vedang Manerikar <ved.manerikar@gmail.com>
2025-03-14 23:11:19 +0530

    fix(io): Remove reflection warnings by using buffered variants

--- src/io/modelcontext/clojure_sdk/io_chan.clj
+++ src/io/modelcontext/clojure_sdk/io_chan.clj
@@ -4,8 +4,7 @@
             [camel-snake-kebab.extras :as cske]
             [clojure.core.async :as async]
             [clojure.java.io :as io]
-            [me.vedang.logger.interface :as log])
-  (:import (java.io InputStream OutputStream)))
+            [me.vedang.logger.interface :as log]))
 
 (set! *warn-on-reflection* true)
 
@@ -18,7 +17,7 @@
 ;; https://modelcontextprotocol.io/specification
 
 (defn ^:private read-message
-  [^InputStream input]
+  [^java.io.BufferedReader input]
   (try (let [content (.readLine input)]
          (log/trace :fn :read-message :line content)
          (json/read-str content))
@@ -33,10 +32,10 @@
 (def ^:private write-lock (Object.))
 
 (defn ^:private write-message
-  [^OutputStream output msg]
+  [^java.io.BufferedWriter output msg]
   (let [content (json/write-str (cske/transform-keys kw->camelCaseString msg))]
     (locking write-lock
-      (doto output (.write content) (.newLine content) (.flush)))))
+      (doto output (.write ^String content) (.newLine) (.flush)))))
 
 (defn input-stream->input-chan
   "Returns a channel which will yield parsed messages that have been read off
@@ -44,27 +43,25 @@
   channel closes, will close the input, but can be determined by `close?`.
 
   Reads in a thread to avoid blocking a go block thread."
-  ([input] (input-stream->input-chan input {}))
-  ([input {:keys [close?], :or {close? true}}]
-   (log/trace :fn :input-stream->input-chan :msg "Creating new input-chan")
-   (let [input (io/input-stream input)
-         messages (async/chan 1)]
-     (async/thread
-       (loop []
-         (let [msg (read-message input)]
-           (cond
-             ;; input closed; also close channel
-             (= msg :parse-error) (do (log/debug :fn :input-stream->input-chan
-                                                 :error true
-                                                 :msg "Parse error or EOF")
-                                      (async/close! messages))
-             :else (do (log/trace :fn :input-stream->input-chan :msg msg)
-                       (if (async/>!! messages msg)
-                         ;; wait for next message
-                         (recur)
-                         ;; messages closed
-                         (when close? (.close input))))))))
-     messages)))
+  [input]
+  (log/trace :fn :input-stream->input-chan :msg "Creating new input-chan")
+  (let [messages (async/chan 1)]
+    ;; close output when channel closes
+    (async/thread
+      (with-open [reader (io/reader (io/input-stream input))]
+        (loop []
+          (let [msg (read-message reader)]
+            (cond
+              ;; input closed; also close channel
+              (= msg :parse-error) (do (log/debug :fn :input-stream->input-chan
+                                                  :error true
+                                                  :msg "Parse error or EOF")
+                                       (async/close! messages))
+              :else (do (log/trace :fn :input-stream->input-chan :msg msg)
+                        (when (async/>!! messages msg)
+                          ;; wait for next message
+                          (recur))))))))
+    messages))
 
 (defn output-stream->output-chan
   "Returns a channel which expects to have messages put on it. nil values are
@@ -73,10 +70,9 @@
 
   Writes in a thread to avoid blocking a go block thread."
   [output]
-  (let [output (io/output-stream output)
-        messages (async/chan 1)]
-    (async/thread (with-open [writer output] ;; close output when channel
-                                             ;; closes
+  (let [messages (async/chan 1)]
+    ;; close output when channel closes
+    (async/thread (with-open [writer (io/writer (io/output-stream output))]
                     (loop []
                       (when-let [msg (async/<!! messages)]
                         (log/trace :fn :output-stream->output-chan :msg msg)
