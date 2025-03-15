1892d76cb2430c64ed3318d71d96fd857f9b95c9
Vedang Manerikar <ved.manerikar@gmail.com>
2025-03-15 23:26:15 +0530

    refactor: Remove dead-code
    I don't use `clojure_sdk.clj` for anything at the moment. I will
    re-create these files if I need them later

--- src/io/modelcontext/clojure_sdk.clj
+++
@@ -1,3 +0,0 @@
-(ns io.modelcontext.clojure-sdk)
-
-(defn foo "I don't do a whole lot." [x] (prn x "Hello, World!"))
--- test/io/modelcontext/clojure_sdk_test.clj
+++
@@ -1,5 +0,0 @@
-(ns io.modelcontext.clojure-sdk-test
-  (:require [clojure.test :refer [deftest is testing]]
-            [io.modelcontext.clojure-sdk :as clojure-sdk]))
-
-(deftest a-test (testing "FIXME, I pass, but do nothing." (is (= 1 1))))
